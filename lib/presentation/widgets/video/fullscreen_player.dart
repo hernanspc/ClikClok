4424935917e4763077949d2e077079e6644f6e90
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-08-21 17:39:41 -0500

    full video

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
@@ -11,7 +12,43 @@ class FullScreenPlayer extends StatelessWidget {
   });
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(strokeWidth: 2),
+          );
+        }
+
+        return AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        );
+      },
+    );
   }
 }
